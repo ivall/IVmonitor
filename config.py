50f7014c2456c9685df84ac6f401018e675b065b
ivall <ivallpl@gmail.com>
2021-02-06 11:40:50 +0100

    Config file

---
+++ config.py
@@ -0,0 +1,24 @@
+# django config
+DJANGO_SECRET_KEY = ''
+
+# recaptcha config
+RECAPTCHA_SECRET_KEY = ''
+
+# email config
+EMAIL_USE_TLS = True
+EMAIL_HOST = ''
+EMAIL_DEFAULT_FROM_EMAIL = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+
+# monitor config
+MONITOR_TYPES = ['website']
+RATE_TIME = range(1,60)  # in minutes
+MAX_USER_MONITORS = 10
+
+# alert config
+ALERT_TYPES = ['email', 'webhook']
+
+# global config
+DOMAIN = ''
